dc53c714f68a9378cb87b7cdc58f7d2ffe02d9d6
Victor Comitti <victor.comitti@hence-analytics.com>
2024-12-26 19:17:15 -0300

    figura 2

--- visualizacoes.R
+++ visualizacoes.R
@@ -2,6 +2,8 @@ library(readxl)
 library(dlyr)
 library(ggplot2)
 library(data.table)
+library(tidyverse)
+library(stringr)
 
 # Figura 1 ---------------------------------------------------------------------
 
@@ -14,7 +16,7 @@ DT <- dados %>%
   setNames(c("Revistas", "Frequencia")) %>%
   data.table()
 
-# Plotar gráfico
+# Gráfico de barras
 ggplot(DT, aes(x = reorder(Revistas, Frequencia), y = Frequencia * 100)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   geom_text(aes(label = paste0(round(Frequencia * 100, 2), "%")),
@@ -27,3 +29,20 @@ ggplot(DT, aes(x = reorder(Revistas, Frequencia), y = Frequencia * 100)) +
 
 # Figura 2 ---------------------------------------------------------------------
 
+df <- dados %>%
+  select(País) %>%
+  mutate(País = str_replace_all(País, " e ", ",")) %>%
+  separate_rows(País, sep = ",") %>%
+  mutate(País = str_trim(País)) %>%
+  mutate(País = str_replace_all(País, "[;:.]", ""))
+
+frequencia <- df %>%
+  drop_na() %>%
+  count(País, sort = TRUE)
+
+ggplot(frequencia, aes(x = reorder(País, n), y = n)) +
+  geom_col(fill = "skyblue") +
+  geom_text(aes(label = n), hjust = -0.1, size = 3.5, color = "black") +
+  coord_flip() +
+  labs(title = "", x = "País", y = "Frequência") +
+  theme_minimal()
